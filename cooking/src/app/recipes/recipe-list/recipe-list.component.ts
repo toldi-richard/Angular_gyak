1cd198400f7d5f900c5676770637c3b8be7887a2
toldi-richard <rtoldi@gmail.com>
2023-02-05 17:22:01 +0100

    Angular halad

--- cooking/src/app/recipes/recipe-list/recipe-list.component.ts
+++ cooking/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is a simply test.', 'https://learnenglishteens.britishcouncil.org/sites/teens/files/a2_writing_a_recipe.jpg')
+  ];
 
+  constructor() { }
+  ngOnInit(): void { }
 }
