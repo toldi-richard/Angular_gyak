64fcd21279c279dcba53d4722617cd0e7a9afef2
Richárd Toldi <toldir@griffsoft.hu>
2023-02-06 13:32:11 +0100

    angular halad

--- cooking/src/app/shopping-list/shopping-list.component.ts
+++ cooking/src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent {
-
+export class ShoppingListComponent implements OnInit{
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 3),
+    new Ingredient('Banana', 5)
+  ];
+  constructor() {
+  }
+  ngOnInit() {
+  }
 }
