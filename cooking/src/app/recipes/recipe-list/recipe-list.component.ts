e1be88b6e2c21e649ada02cf7776df303409685d
toldi-richard <rtoldi@gmail.com>
2023-02-20 10:28:50 +0100

    angular halad 89. lecke jön

--- cooking/src/app/recipes/recipe-list/recipe-list.component.ts
+++ cooking/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,8 +8,8 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A test recipe', 'This is a simply test.', 'https://s7d1.scene7.com/is/image/mcdonalds/2_Pub_Commitment_574x384:2-column-desktop?resmode=sharp2'),
-    new Recipe('A test recipe', 'This is a simply test.', 'https://s7d1.scene7.com/is/image/mcdonalds/2_Pub_Commitment_574x384:2-column-desktop?resmode=sharp2')
+    new Recipe('A test recipe 1', 'This is a simply test 1.', 'https://s7d1.scene7.com/is/image/mcdonalds/2_Pub_Commitment_574x384:2-column-desktop?resmode=sharp2'),
+    new Recipe('A test recipe 2' , 'This is a simply test 2.', 'https://s7d1.scene7.com/is/image/mcdonalds/2_Pub_Commitment_574x384:2-column-desktop?resmode=sharp2')
   ];
 
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
